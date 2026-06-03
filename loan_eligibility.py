42c45e82b44b692d98824e878107ce5ba6bbf4b7
Lalitsirvi2010 <alalit2010@gmail.com>
2026-06-03 14:00:31 +0530

    Delete loan_eligibility.py

--- loan_eligibility.py
+++
@@ -1,6 +0,0 @@
-age = int(input("Enter your age: "))
-monthly_income = int(input("Enter your monthly income (in figures): "))
-credit_score = int(input("Enter your credit score: "))
-existing_loan_amount = int(input("Enter existing loan amount (in figures): "))
-employment_type = str(input('Enter employment type (student , salaried , self-employed): ' )) .lower().replace(' ', '-')
-
