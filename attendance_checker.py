8fe7e36f0ad301cde21775c030aa8d1ac9630dbb
Lalitsirvi2010 <alalit2010@gmail.com>
2026-06-08 15:00:15 +0530

    .

---
+++ attendance_checker.py
@@ -0,0 +1,15 @@
+attendance =  float(input("Enter your attendance: "))
+if attendance < 0 :
+    print("Invalid attendance.")
+elif attendance >= 100 :
+    print("Invalid attendance.")
+elif attendance >= 90 :
+    print("Excellent attendance, allowed for exam.")
+elif attendance >= 75 :
+    print('Good attendance, allowed for exam.')
+elif attendance >= 65 :
+    print("Warning, improve attendance.")
+elif attendance  >= 50 :
+    print("Serious warning, meet teacher.")
+else :
+    print("Not allowed for exam.")
